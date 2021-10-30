c87426f6eb9e27a6f5e4b21127c35b4b2fe0a785
Erik Gunnarsson <erik.gunarson@gmail.com>
2021-10-30 18:59:42 +0200

    Added JIT based on kaleidoscope

---
+++ lib/JIT.cpp
@@ -0,0 +1,65 @@
+
+#pragma warning(push, 0)
+
+#include "llvm/ExecutionEngine/Orc/CompileUtils.h"
+#include "llvm/ExecutionEngine/Orc/ExecutionUtils.h"
+#include "llvm/ExecutionEngine/SectionMemoryManager.h"
+
+#pragma warning(pop)
+
+#include "JIT.h"
+
+
+namespace llfp
+{
+
+JIT::JIT(std::unique_ptr<llvm::orc::ExecutionSession> ES, llvm::orc::JITTargetMachineBuilder JTMB, llvm::DataLayout DL) :
+    ES(std::move(ES)),
+    DL(std::move(DL)),
+    Mangle(*this->ES, this->DL),
+    ObjectLayer(*this->ES, []() { return std::make_unique<llvm::SectionMemoryManager>(); }),
+    CompileLayer(*this->ES, ObjectLayer, std::make_unique<llvm::orc::ConcurrentIRCompiler>(std::move(JTMB))),
+    MainJD(this->ES->createBareJITDylib("<main>"))
+{
+    MainJD.addGenerator(llvm::cantFail(llvm::orc::DynamicLibrarySearchGenerator::GetForCurrentProcess(DL.getGlobalPrefix())));
+    ObjectLayer.setOverrideObjectFlagsWithResponsibilityFlags(true);
+}
+
+JIT::~JIT()
+{
+    if (auto Err = ES->endSession())
+        ES->reportError(std::move(Err));
+}
+
+llvm::Expected<std::unique_ptr<JIT>> JIT::Create()
+{
+    auto EPC = llvm::orc::SelfExecutorProcessControl::Create();
+    if (!EPC)
+        return EPC.takeError();
+
+    auto ES = std::make_unique<llvm::orc::ExecutionSession>(std::move(*EPC));
+
+    llvm::orc::JITTargetMachineBuilder JTMB(
+        ES->getExecutorProcessControl().getTargetTriple());
+
+    auto DL = JTMB.getDefaultDataLayoutForTarget();
+    if (!DL)
+        return DL.takeError();
+
+    return std::make_unique<JIT>(std::move(ES), std::move(JTMB),
+        std::move(*DL));
+}
+
+llvm::Error JIT::addModule(llvm::orc::ThreadSafeModule TSM, llvm::orc::ResourceTrackerSP RT)
+{
+    if (!RT)
+        RT = MainJD.getDefaultResourceTracker();
+    return CompileLayer.add(RT, std::move(TSM));
+}
+
+llvm::Expected<llvm::JITEvaluatedSymbol> JIT::lookup(llvm::StringRef Name)
+{
+    return ES->lookup({ &MainJD }, Mangle(Name));
+}
+
+} // namespace llfp
---
+++ lib/include/JIT.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <memory>
+
+#pragma warning(push, 0)
+
+#include "llvm/ExecutionEngine/Orc/Core.h"
+#include "llvm/ExecutionEngine/Orc/IRCompileLayer.h"
+#include "llvm/ExecutionEngine/Orc/Mangling.h"
+#include "llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h"
+#include "llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h"
+#include "llvm/IR/DataLayout.h"
+
+#pragma warning(pop)
+
+
+namespace llfp
+{
+
+class JIT
+{
+    std::unique_ptr<llvm::orc::ExecutionSession> ES;
+
+    llvm::DataLayout DL;
+    llvm::orc::MangleAndInterner Mangle;
+
+    llvm::orc::RTDyldObjectLinkingLayer ObjectLayer;
+    llvm::orc::IRCompileLayer CompileLayer;
+
+    llvm::orc::JITDylib& MainJD;
+
+public:
+
+    JIT(std::unique_ptr<llvm::orc::ExecutionSession> ES,  llvm::orc::JITTargetMachineBuilder JTMB, llvm::DataLayout DL);
+    ~JIT();
+
+    static llvm::Expected<std::unique_ptr<JIT>> Create();
+
+    const llvm::DataLayout& getDataLayout() const { return DL; }
+    llvm::orc::JITDylib&    getMainJITDylib() { return MainJD; }
+
+    llvm::Error addModule(llvm::orc::ThreadSafeModule TSM, llvm::orc::ResourceTrackerSP RT = nullptr);
+
+    llvm::Expected<llvm::JITEvaluatedSymbol> lookup(llvm::StringRef Name);
+};
+
+} // namespace llfp
